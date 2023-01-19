527c844feea4324c311306b664d0cc992ef4c097
Yauheni Shaikouski <47056578+evgenyshaikovskiy@users.noreply.github.com>
2023-01-19 19:16:23 +0300

    merge routing feature (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./components/routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/routes/navigation/navigation.component";
 
 const App = () => {
-  return <Directory></Directory>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="/shop" element={<div>IAM SHOP</div>}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
