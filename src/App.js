aba5f79c6cb119a83ec4655a17b9a8cd54b02fc1
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2023-01-19 19:15:32 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import Home from "./components/routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/routes/navigation/navigation.component";
 
 const App = () => {
-  return <Home></Home>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="/shop" element={<div>IAM SHOP</div>}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
